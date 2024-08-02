e32835560544896ac9bb88f7c8100e5b7c14b5be
unknown <tejadahyllamae@gmail.com>
2024-08-02 17:39:17 +0800

    First commit....

---
+++ test.py
@@ -0,0 +1,18 @@
+print('Hello World')
+
+number_one = 10
+number_two = 13
+
+first_name = 'Hylla Mae'
+
+char_sample = 'c'
+
+people_age = {
+    'Hylla Mae': 21,
+    'julie': 20,
+    'kazyll': 20
+}
+
+total = number_one + number_two
+
+print('The result is', total)
